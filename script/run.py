4ce1a536dd5c6649e89f4a8bba7e39f0089661e9
TroyeJames9 <130838331+TroyeJames9@users.noreply.github.com>
2024-01-23 10:26:36 +0800

    修改_项目架构

--- script/run.py
+++ script/run.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from lfasr_new import downloadOrderResult, getTransferResult, getCutPoint, getCpTimestamp, cutAudio, extractLyrics, \
     gbkXfrFstLetter, getPerWordTime
 from score.articulation_analysis import calculate_cosine_similarity, kmeanCatogery
-from setting import LFASR_APP_ID, LFASR_SECRETKEY, FILE, ROOT, UPLOAD_FILE_DIR, DOWNLOAD_DIR, LYRICS_DIR, AUDIO_DIR, \
+from setting import LFASR_APP_ID, LFASR_SECRETKEY, ROOT, UPLOAD_FILE_DIR, DOWNLOAD_DIR, LYRICS_DIR, AUDIO_DIR, \
     OUTPUT_JSON_NAME, OUTPUT_AUDIO_NAME
 
 
--- script/setting.py
+++ script/setting.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+
+"""所有跨文件的全局变量由本模块计算并赋值
+
+其他模块需要使用本模块全局变量时，在模块开头导入本模块即可
+例子：
+    from setting import ROOT,LFASR_APP_ID,LFASR_SECRETKEY...
+
+"""
+
 import os
 import sys
 import re
@@ -15,6 +24,8 @@ if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
+DATA_DIR = ROOT / "data"
+EIGEN_DIR = ROOT / "eigen_json"
 UPLOAD_FILE_DIR = ROOT / "audio"
 DOWNLOAD_DIR = ROOT / 'resultJson'
 LYRICS_DIR = ROOT / "lyrics"
