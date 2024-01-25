581a8b6897896ee70d93b9797a8b891e137fab94
TroyeJames9 <130838331+TroyeJames9@users.noreply.github.com>
2024-01-25 16:32:52 +0800

    black_测试

--- script/preprocess/lfasr_new.py
+++ script/preprocess/lfasr_new.py
@@ -22,10 +22,10 @@ from pathlib import Path
 from pydub import AudioSegment
 from pypinyin import pinyin, lazy_pinyin, Style
 
-lfasr_host = 'https://raasr.xfyun.cn/v2/api'
+lfasr_host = "https://raasr.xfyun.cn/v2/api"
 # 请求的接口名
-api_upload = '/upload'
-api_get_result = '/getResult'
+api_upload = "/upload"
+api_get_result = "/getResult"
 
 
 class RequestApi(object):
@@ -60,16 +60,13 @@ class RequestApi(object):
         appid = self.appid
         secret_key = self.secret_key
         m2 = hashlib.md5()
-        m2.update((appid + self.ts).encode('utf-8'))
+        m2.update((appid + self.ts).encode("utf-8"))
         md5 = m2.hexdigest()
-        md5 = bytes(md5, encoding='utf-8')
+        md5 = bytes(md5, encoding="utf-8")
         # 以secret_key为key, 上面的md5为msg， 使用hashlib.sha1加密结果为signa
-        signa = hmac.new(
-            secret_key.encode('utf-8'),
-            md5,
-            hashlib.sha1).digest()
+        signa = hmac.new(secret_key.encode("utf-8"), md5, hashlib.sha1).digest()
         signa = base64.b64encode(signa)
-        signa = str(signa, 'utf-8')
+        signa = str(signa, "utf-8")
         return signa
 
     def upload(self):
@@ -85,9 +82,9 @@ class RequestApi(object):
         file_name = os.path.basename(upload_file_path)
 
         param_dict = {}
-        param_dict['appId'] = self.appid
-        param_dict['signa'] = self.signa
-        param_dict['ts'] = self.ts
+        param_dict["appId"] = self.appid
+        param_dict["signa"] = self.signa
+        param_dict["ts"] = self.ts
         param_dict["fileSize"] = file_len
         param_dict["fileName"] = file_name
         param_dict["duration"] = "200"  # 没严格限制，无需修改
@@ -104,16 +101,13 @@ class RequestApi(object):
         # param_dict["pd"] = ""
         # param_dict["audioMode"] = ""
         print("upload param：", param_dict)
-        data = open(upload_file_path, 'rb').read(file_len)
+        data = open(upload_file_path, "rb").read(file_len)
 
         response = requests.post(
-            url=lfasr_host +
-            api_upload +
-            "?" +
-            urllib.parse.urlencode(param_dict),
-            headers={
-                "Content-type": "application/json"},
-            data=data)
+            url=lfasr_host + api_upload + "?" + urllib.parse.urlencode(param_dict),
+            headers={"Content-type": "application/json"},
+            data=data,
+        )
         print("upload_url:", response.request.url)
         upload_result = json.loads(response.text)
         print("upload resp:", upload_result)
@@ -131,13 +125,13 @@ class RequestApi(object):
             “https://www.xfyun.cn/doc/asr/ifasr_new/API.html” 。
         """
         uploadresp = self.upload()
-        orderId = uploadresp['content']['orderId']
+        orderId = uploadresp["content"]["orderId"]
         param_dict = {}
-        param_dict['appId'] = self.appid
-        param_dict['signa'] = self.signa
-        param_dict['ts'] = self.ts
-        param_dict['orderId'] = orderId
-        param_dict['resultType'] = "transfer"
+        param_dict["appId"] = self.appid
+        param_dict["signa"] = self.signa
+        param_dict["ts"] = self.ts
+        param_dict["orderId"] = orderId
+        param_dict["resultType"] = "transfer"
         print("")
         print("get_result part：")
         print("get_result parm：", param_dict)
@@ -145,16 +139,16 @@ class RequestApi(object):
         # 建议使用回调的方式查询结果，查询接口有请求频率限制
         while status == 3:
             response = requests.post(
-                url=lfasr_host +
-                api_get_result +
-                "?" +
-                urllib.parse.urlencode(param_dict),
-                headers={
-                    "Content-type": "application/json"})
+                url=lfasr_host
+                + api_get_result
+                + "?"
+                + urllib.parse.urlencode(param_dict),
+                headers={"Content-type": "application/json"},
+            )
             # print("get_result_url:",response.request.url)
             result = json.loads(response.text)
             print(result)
-            status = result['content']['orderInfo']['status']
+            status = result["content"]["orderInfo"]["status"]
             print("status=", status)
             if status == 4 and status == -1:
                 break
@@ -163,13 +157,14 @@ class RequestApi(object):
 
 
 def downloadOrderResult(
-        appid: str,
-        secret_key: str,
-        lyrics_dir: Path,
-        lyrics_file_name: str,
-        upload_file_path: Path,
-        download_dir: Path,
-        output_file_name: str) -> dict:
+    appid: str,
+    secret_key: str,
+    lyrics_dir: Path,
+    lyrics_file_name: str,
+    upload_file_path: Path,
+    download_dir: Path,
+    output_file_name: str,
+) -> dict:
     """调用RequestApi类来获取音频转写的json格式字符串结果。
 
     首先提取音频名，结合output_file_name来对json结果文件进行命名，
@@ -200,7 +195,7 @@ def downloadOrderResult(
     """
     file_name = os.path.basename(upload_file_path)
     real_file_name = re.search(r"^(.*?)\.", file_name).group(1)
-    new_output_file_name = real_file_name + '_' + output_file_name
+    new_output_file_name = real_file_name + "_" + output_file_name
     download_path = download_dir / new_output_file_name
     # 判断音频的json文件是否已经存在，已存在则不执行下载操作
     if not os.path.exists(download_path):
@@ -212,13 +207,15 @@ def downloadOrderResult(
         else:
             print(f"Path '{download_dir}' already exists.")
         hotword = getHotWords(lyrics_dir=lyrics_dir, file_name=lyrics_file_name)
-        api = RequestApi(appid=appid,
-                         secret_key=secret_key,
-                         upload_file_path=upload_file_path,
-                         hotword=hotword)
+        api = RequestApi(
+            appid=appid,
+            secret_key=secret_key,
+            upload_file_path=upload_file_path,
+            hotword=hotword,
+        )
         transfer_result = api.get_result()
         # 获取transfer_result结果中content下orderResult的字符串内容
-        orderResult_str = transfer_result['content']['orderResult']
+        orderResult_str = transfer_result["content"]["orderResult"]
         orderResult_json = json.loads(orderResult_str)
         lattice_json = orderResult_json["lattice"]
         # 将JSON数据保存到本地
@@ -227,7 +224,7 @@ def downloadOrderResult(
         print(f"File '{download_path}' created!")
     else:
         print(f"File '{download_path}' already exists!Download is close...")
-    with open(download_path, 'r', encoding='gbk') as file:
+    with open(download_path, "r", encoding="gbk") as file:
         lattice_json = json.load(file)
     return lattice_json
 
@@ -255,13 +252,11 @@ def gbkXfrFstLetter(gbk_str: str, style: int) -> str:
         return gbk_str
     elif style == 1:
         pinyin_list = lazy_pinyin(gbk_str)
-        pinyin_result = ' '.join(''.join(inner_list)
-                                 for inner_list in pinyin_list)
+        pinyin_result = " ".join("".join(inner_list) for inner_list in pinyin_list)
     elif style == 2:
         pinyin_list = pinyin(gbk_str, style=Style.FIRST_LETTER)
-        pinyin_result = ''.join(''.join(inner_list)
-                                for inner_list in pinyin_list)
-    reg_pinyin_result = re.sub(r'[^a-z\s]', '', pinyin_result)
+        pinyin_result = "".join("".join(inner_list) for inner_list in pinyin_list)
+    reg_pinyin_result = re.sub(r"[^a-z\s]", "", pinyin_result)
     return reg_pinyin_result
 
 
@@ -283,16 +278,14 @@ def extractValues(data: dict, style: int) -> list:
     # 如果data是一个列表，遍历列表中的每个元素，并递归调用extractValues函数，将返回的值扩展到w_values列表中
     if isinstance(data, list):
         for item in data:
-            w_values.extend(
-                extractValues(data=item, style=style))
+            w_values.extend(extractValues(data=item, style=style))
     # 如果data是一个字典，遍历字典中的每个键值对
     elif isinstance(data, dict):
         for key, value in data.items():
             if key == "w":
                 w_values.append(gbkXfrFstLetter(value, style=style))
             else:
-                w_values.extend(
-                    extractValues(data=value, style=style))
+                w_values.extend(extractValues(data=value, style=style))
     return w_values
 
 
@@ -319,9 +312,7 @@ def getTransferResult(transfer_json: dict, style: int) -> str:
         concatenated_string = "".join(w_values)
         w_list.append(concatenated_string)
     transfer_result = "".join(w_list)
-    print(
-        '\n the transfer result is：\n',
-        transfer_result)
+    print("\n the transfer result is：\n", transfer_result)
     return transfer_result
 
 
@@ -340,10 +331,10 @@ def extractLyrics(lyrics_dir: Path, file_name: str, style: int) -> str:
         歌词的指定风格字符串。
     """
     lyrics_path = lyrics_dir / file_name
-    with open(lyrics_path, 'r', encoding='utf-8') as file:
+    with open(lyrics_path, "r", encoding="utf-8") as file:
         raw_lyrics = file.read()
-    pattern = re.compile(r'[^\u4e00-\u9fa5]')
-    lyrics = re.sub(pattern, '', raw_lyrics)
+    pattern = re.compile(r"[^\u4e00-\u9fa5]")
+    lyrics = re.sub(pattern, "", raw_lyrics)
     lyrics = gbkXfrFstLetter(lyrics, style=style)
     return lyrics
 
@@ -364,21 +355,18 @@ def getHotWords(lyrics_dir: Path, file_name: str) -> str:
         由|分割的hotword正则表达式字符串。
     """
     lyrics_path = lyrics_dir / file_name
-    with open(lyrics_path, 'r', encoding='utf-8') as file:
+    with open(lyrics_path, "r", encoding="utf-8") as file:
         raw_lyrics = file.read()
-    pattern = re.compile(r'[^\u4e00-\u9fa5\n]+')
-    result = pattern.sub('', raw_lyrics).replace('\n', '|')
-    result = re.sub(r'\|+', '|', result)
+    pattern = re.compile(r"[^\u4e00-\u9fa5\n]+")
+    result = pattern.sub("", raw_lyrics).replace("\n", "|")
+    result = re.sub(r"\|+", "|", result)
     return result
 
 
 # TODO: 仍然受限于容错率问题，需要考虑使用编辑距离来编写一个新的索引函数。（也可以在findSubstringIndex函数基础上进行改版）
 def findSubstringIndex(
-        full_str: str,
-        lyrics: str,
-        threshold=0.6,
-        is_end=False,
-        match_str_size=20) -> int:
+    full_str: str, lyrics: str, threshold=0.6, is_end=False, match_str_size=20
+) -> int:
     """通过歌词与API转写结果的元素比对确定音频切割在转写结果中的起始索引和终点索引。
 
     给定API转写结果字符串与歌词字符串，取歌词开头（结尾）前match_str_size个字符作为匹配字符串，
@@ -404,34 +392,28 @@ def findSubstringIndex(
         返回音频切割所需的起始（终点）索引。
     """
     if is_end:
-        match_str = lyrics[-1 * match_str_size:]
+        match_str = lyrics[-1 * match_str_size :]
         range_list = reversed(range(len(full_str) - match_str_size + 1))
     else:
         match_str = lyrics[:match_str_size]
         range_list = range(len(full_str) - match_str_size + 1)
     for i in range_list:
-        substring = full_str[i:i + match_str_size]
-        match_percentage = sum(
-            1 for x,
-            y in zip(
-                substring,
-                match_str) if x == y) / match_str_size
+        substring = full_str[i : i + match_str_size]
+        match_percentage = (
+            sum(1 for x, y in zip(substring, match_str) if x == y) / match_str_size
+        )
         if match_percentage >= threshold:
             if is_end:
                 return i + match_str_size - 1
             else:
                 return i
     raise ValueError(
-        f"Failed to match the head index of string {'match_str'}, please try again")
+        f"Failed to match the head index of string {'match_str'}, please try again"
+    )
 
 
 # TODO: 仍然受限于容错率问题，需要考虑使用编辑距离来编写一个新的索引函数。（也可以在findSubstringIndex函数进行改版）
-def getCutPoint(
-        lyrics_dir,
-        w_str_result,
-        file_name,
-        threshold,
-        match_str_size=20):
+def getCutPoint(lyrics_dir, w_str_result, file_name, threshold, match_str_size=20):
     # lyrics = extractLyrics(
     #     lyrics_dir=lyrics_dir,
     #     file_name=file_name,
@@ -455,11 +437,14 @@ def getCutPoint(
         "start_cut_point_index is",
         start_cut_point_index,
         ",end_cut_point_index is",
-        end_cut_point_index)
+        end_cut_point_index,
+    )
     return start_cut_point_index, end_cut_point_index
 
 
-def findWbValue(sentence: dict, current_index: int, target_index: int, is_end=False) -> tuple:
+def findWbValue(
+    sentence: dict, current_index: int, target_index: int, is_end=False
+) -> tuple:
     """根据目标索引来确定索引所对应的时间戳。
 
     遍历传入的sentence的字典，来提取"w"值对应的wb（we）的取值，当加上当前"w"字符串长度后的current_index
@@ -486,10 +471,10 @@ def findWbValue(sentence: dict, current_index: int, target_index: int, is_end=Fa
         else:
             wb_value = word["wb"] * 10
         cw_list = word["cw"][0]
-        if cw_list["wp"] in ['n', 's']:
+        if cw_list["wp"] in ["n", "s"]:
             w_value = gbkXfrFstLetter(word["cw"][0]["w"], style=2)
         else:
-            w_value = ''
+            w_value = ""
         current_index += len(w_value)
         if current_index >= target_index + 1:
             break
@@ -515,7 +500,11 @@ def getCpTimestamp(transfer_json: dict, target_index: int, is_end=False) -> floa
     for element in transfer_json:
         json_best = json.loads(element["json_1best"])
         current_index, wb_values = findWbValue(
-            sentence=json_best["st"], current_index=current_index, target_index=target_index, is_end=is_end)
+            sentence=json_best["st"],
+            current_index=current_index,
+            target_index=target_index,
+            is_end=is_end,
+        )
         if current_index >= target_index + 1:
             cut_point_t = int(json_best["st"]["bg"]) + wb_values
             break
@@ -541,18 +530,15 @@ def getPerWordTime(transfer_json: dict) -> list:
             wb_value = word["wb"] * 10
             w_time = (sentence_bg + wb_value) / 1000
             cw_list = word["cw"][0]
-            if cw_list["wp"] in ['n', 's']:
+            if cw_list["wp"] in ["n", "s"]:
                 w_value = gbkXfrFstLetter(word["cw"][0]["w"], style=0)
                 wt_list.append({w_value: w_time})
     return wt_list
 
 
 def cutAudio(
-        start_time=0.0,
-        end_time=None,
-        output_dir=None,
-        output_audio=None,
-        input_audio=None):
+    start_time=0.0, end_time=None, output_dir=None, output_audio=None, input_audio=None
+):
     """给定时间戳对音频进行切割
 
     参数：
@@ -572,7 +558,7 @@ def cutAudio(
 
     """
     file_name = os.path.basename(input_audio)
-    output_name = file_name + '_' + output_audio
+    output_name = file_name + "_" + output_audio
     output_path = output_dir / output_name
     print(f"Start download '{output_path}' ...")
     if not os.path.exists(output_dir):
@@ -582,9 +568,9 @@ def cutAudio(
     duration_s = len(audio) / 1000
     if end_time is None:
         end_time = duration_s
-    segment = audio[start_time * 1000:end_time * 1000]
+    segment = audio[start_time * 1000 : end_time * 1000]
     segment.export(output_path, format="mp3")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     doctest.testmod()
--- script/run/run.py
+++ script/run/run.py
@@ -5,29 +5,45 @@ import glob
 import time
 import concurrent.futures
 import asyncio
-from temp.lfasr_new import downloadOrderResult, getTransferResult, getCutPoint, getCpTimestamp, cutAudio, extractLyrics, \
-    gbkXfrFstLetter, getPerWordTime
+from temp.lfasr_new import (
+    downloadOrderResult,
+    getTransferResult,
+    getCutPoint,
+    getCpTimestamp,
+    cutAudio,
+    extractLyrics,
+    gbkXfrFstLetter,
+    getPerWordTime,
+)
 from score.articulation_analysis import calculate_cosine_similarity, kmeanCatogery
-from setting import LFASR_APP_ID, LFASR_SECRETKEY, UPLOAD_FILE_DIR, DOWNLOAD_DIR, LYRICS_DIR, AUDIO_DIR, \
-    OUTPUT_JSON_NAME, OUTPUT_AUDIO_NAME
+from setting import (
+    LFASR_APP_ID,
+    LFASR_SECRETKEY,
+    UPLOAD_FILE_DIR,
+    DOWNLOAD_DIR,
+    LYRICS_DIR,
+    AUDIO_DIR,
+    OUTPUT_JSON_NAME,
+    OUTPUT_AUDIO_NAME,
+)
 
 
 def extractLyricsPart(
-        upload_file_path=UPLOAD_FILE_DIR / 'song_demo.mp3',
-        lyrics_file_name='song_demo.txt',
-        is_cut=True,
-        is_download_seg=False,
-        cut_style=2,
-        extract_style=0,
-        match_str_size=20,
-        threshold=0.6,
-        app_id=LFASR_APP_ID,
-        secret_key=LFASR_SECRETKEY,
-        download_dir=DOWNLOAD_DIR,
-        output_json_name=OUTPUT_JSON_NAME,
-        lyrics_dir=LYRICS_DIR,
-        audio_dir=AUDIO_DIR,
-        output_audio_name=OUTPUT_AUDIO_NAME
+    upload_file_path=UPLOAD_FILE_DIR / "song_demo.mp3",
+    lyrics_file_name="song_demo.txt",
+    is_cut=True,
+    is_download_seg=False,
+    cut_style=2,
+    extract_style=0,
+    match_str_size=20,
+    threshold=0.6,
+    app_id=LFASR_APP_ID,
+    secret_key=LFASR_SECRETKEY,
+    download_dir=DOWNLOAD_DIR,
+    output_json_name=OUTPUT_JSON_NAME,
+    lyrics_dir=LYRICS_DIR,
+    audio_dir=AUDIO_DIR,
+    output_audio_name=OUTPUT_AUDIO_NAME,
 ):
     """传入音频后可根据歌词文件级"""
     result_json = downloadOrderResult(
@@ -37,76 +53,91 @@ def extractLyricsPart(
         lyrics_file_name=lyrics_file_name,
         upload_file_path=upload_file_path,
         download_dir=download_dir,
-        output_file_name=output_json_name)
+        output_file_name=output_json_name,
+    )
     w_str_result = getTransferResult(result_json, style=cut_style)
 
     raw_extract_result = getTransferResult(result_json, style=extract_style)
-    pattern = re.compile(r'[^\u4e00-\u9fa5]')
-    extract_result = re.sub(pattern, '', raw_extract_result)
+    pattern = re.compile(r"[^\u4e00-\u9fa5]")
+    extract_result = re.sub(pattern, "", raw_extract_result)
     start_cut_point, end_cut_point = 0, len(extract_result)
 
     if is_cut:
         start_cut_point, end_cut_point = getCutPoint(
-            w_str_result=w_str_result, file_name=lyrics_file_name, match_str_size=match_str_size, lyrics_dir=lyrics_dir,
-            threshold=threshold)
+            w_str_result=w_str_result,
+            file_name=lyrics_file_name,
+            match_str_size=match_str_size,
+            lyrics_dir=lyrics_dir,
+            threshold=threshold,
+        )
         if is_download_seg:
-            s_cut_point_t = getCpTimestamp(
-                result_json,
-                start_cut_point,
-                is_end=False) - 0.2
-            e_cut_point_t = getCpTimestamp(
-                result_json,
-                end_cut_point,
-                is_end=True) + 0.2
+            s_cut_point_t = (
+                getCpTimestamp(result_json, start_cut_point, is_end=False) - 0.2
+            )
+            e_cut_point_t = (
+                getCpTimestamp(result_json, end_cut_point, is_end=True) + 0.2
+            )
             print(
-                "start time is", round(s_cut_point_t, 1),
-                "s,end_time is", round(e_cut_point_t, 1), "s")
+                "start time is",
+                round(s_cut_point_t, 1),
+                "s,end_time is",
+                round(e_cut_point_t, 1),
+                "s",
+            )
             cutAudio(
                 start_time=s_cut_point_t,
                 end_time=e_cut_point_t,
                 output_dir=audio_dir,
                 output_audio=output_audio_name,
-                input_audio=upload_file_path)
-    lyrics_part_str = extract_result[int(
-        start_cut_point):int(end_cut_point + 1)]
+                input_audio=upload_file_path,
+            )
+    lyrics_part_str = extract_result[int(start_cut_point) : int(end_cut_point + 1)]
     return lyrics_part_str
 
 
 def calcCosineSimilarity(
-        input_audio_str,
-        lyrics_dir=LYRICS_DIR,
-        lyrics_file_name='song_demo.txt',
-        style=1,
-        vectorizer_type=0):
+    input_audio_str,
+    lyrics_dir=LYRICS_DIR,
+    lyrics_file_name="song_demo.txt",
+    style=1,
+    vectorizer_type=0,
+):
     input_audio_str = gbkXfrFstLetter(input_audio_str, style=1)
     lyrics = extractLyrics(
-        lyrics_dir=lyrics_dir,
-        file_name=lyrics_file_name,
-        style=style)
+        lyrics_dir=lyrics_dir, file_name=lyrics_file_name, style=style
+    )
     similarity_score = calculate_cosine_similarity(
-        input_audio_str, lyrics, vectorizer_type=vectorizer_type)
+        input_audio_str, lyrics, vectorizer_type=vectorizer_type
+    )
     return similarity_score
 
 
 def loopArticulationAnalysisV1(audio_song_name):
     folder_path = UPLOAD_FILE_DIR / audio_song_name
-    lyrics_file_name = audio_song_name + '.txt'
-    mp3_files = glob.glob(os.path.join(folder_path, '*.mp3'))
+    lyrics_file_name = audio_song_name + ".txt"
+    mp3_files = glob.glob(os.path.join(folder_path, "*.mp3"))
     for file_path in mp3_files:
-        lyrics_part_str = extractLyricsPart(upload_file_path=file_path,
-                                            lyrics_file_name=lyrics_file_name,
-                                            is_cut=False)
+        lyrics_part_str = extractLyricsPart(
+            upload_file_path=file_path, lyrics_file_name=lyrics_file_name, is_cut=False
+        )
         print(
             calcCosineSimilarity(
                 input_audio_str=lyrics_part_str,
                 lyrics_file_name=lyrics_file_name,
-                vectorizer_type=0))
+                vectorizer_type=0,
+            )
+        )
 
 
-async def asyncioExtractLyricsPart(upload_file_path, lyrics_file_name, is_cut, semaphore):
+async def asyncioExtractLyricsPart(
+    upload_file_path, lyrics_file_name, is_cut, semaphore
+):
     async with semaphore:
-        return await extractLyricsPart(upload_file_path=upload_file_path, lyrics_file_name=lyrics_file_name,
-                                       is_cut=is_cut)
+        return await extractLyricsPart(
+            upload_file_path=upload_file_path,
+            lyrics_file_name=lyrics_file_name,
+            is_cut=is_cut,
+        )
 
 
 async def concurrentTask(upload_file_paths, lyrics_file_name, is_cut, max_concurrency):
@@ -117,7 +148,10 @@ async def concurrentTask(upload_file_paths, lyrics_file_name, is_cut, max_concur
             upload_file_path=upload_file_path,
             lyrics_file_name=lyrics_file_name,
             is_cut=is_cut,
-            semaphore=semaphore) for upload_file_path in upload_file_paths]
+            semaphore=semaphore,
+        )
+        for upload_file_path in upload_file_paths
+    ]
 
     results = await asyncio.gather(*tasks)
 
@@ -126,39 +160,41 @@ async def concurrentTask(upload_file_paths, lyrics_file_name, is_cut, max_concur
 
 def asyncArticulationAnalysisV2(audio_song_name):
     folder_path = UPLOAD_FILE_DIR / audio_song_name
-    lyrics_file_name = audio_song_name + '.txt'
-    mp3_files = glob.glob(os.path.join(folder_path, '*.mp3'))
+    lyrics_file_name = audio_song_name + ".txt"
+    mp3_files = glob.glob(os.path.join(folder_path, "*.mp3"))
     loop = asyncio.get_event_loop()
     loop.run_until_complete(
         concurrentTask(
             upload_file_paths=mp3_files,
             lyrics_file_name=lyrics_file_name,
             is_cut=False,
-            max_concurrency=18))
+            max_concurrency=18,
+        )
+    )
 
 
 def threadProcess(
-        upload_file_path,
-        lyrics_file_name,
-        is_cut,
-        is_download_seg,
-        vectorizer_type=0):
-    lyrics_part_str = extractLyricsPart(upload_file_path=upload_file_path,
-                                        lyrics_file_name=lyrics_file_name,
-                                        is_cut=is_cut,
-                                        is_download_seg=is_download_seg)
+    upload_file_path, lyrics_file_name, is_cut, is_download_seg, vectorizer_type=0
+):
+    lyrics_part_str = extractLyricsPart(
+        upload_file_path=upload_file_path,
+        lyrics_file_name=lyrics_file_name,
+        is_cut=is_cut,
+        is_download_seg=is_download_seg,
+    )
     file_name = os.path.basename(upload_file_path)
     cs_result = calcCosineSimilarity(
         input_audio_str=lyrics_part_str,
         lyrics_file_name=lyrics_file_name,
-        vectorizer_type=vectorizer_type)
+        vectorizer_type=vectorizer_type,
+    )
     return {file_name: cs_result}
 
 
 def threadArticulationAnalysisV3(audio_song_name):
     folder_path = UPLOAD_FILE_DIR / audio_song_name
-    lyrics_file_name = audio_song_name + '.txt'
-    mp3_files = glob.glob(os.path.join(folder_path, '*.mp3'))
+    lyrics_file_name = audio_song_name + ".txt"
+    mp3_files = glob.glob(os.path.join(folder_path, "*.mp3"))
     with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
         # 使用map方法并发执行函数
         upload_file_paths = mp3_files
@@ -168,32 +204,34 @@ def threadArticulationAnalysisV3(audio_song_name):
                     lyrics_file_name=lyrics_file_name,
                     is_cut=False,
                     is_download_seg=False,
-                    upload_file_path=x),
-                upload_file_paths))
+                    upload_file_path=x,
+                ),
+                upload_file_paths,
+            )
+        )
     return result_list
 
 
 def run():
-    score_dir_list = threadArticulationAnalysisV3('fenHongSeDeHuiYi')
+    score_dir_list = threadArticulationAnalysisV3("fenHongSeDeHuiYi")
     labels = kmeanCatogery(score_dir_list=score_dir_list, cat_num=4)
     return labels
 
 
 def test_loop_vs_multithread():
     start_time_V1 = time.time()
-    loopArticulationAnalysisV1('fenHongSeDeHuiYi')
+    loopArticulationAnalysisV1("fenHongSeDeHuiYi")
     end_time_V1 = time.time()
     input("请删除resultJson里由v1生成的json，删除完输入y")
     start_time_V3 = time.time()
-    threadArticulationAnalysisV3('fenHongSeDeHuiYi')
+    threadArticulationAnalysisV3("fenHongSeDeHuiYi")
     end_time_V3 = time.time()
     print(
-        'v1 consume',
-        end_time_V1 -
-        start_time_V1,
-        '\nv3 consume',
-        end_time_V3 -
-        start_time_V3)
+        "v1 consume",
+        end_time_V1 - start_time_V1,
+        "\nv3 consume",
+        end_time_V3 - start_time_V3,
+    )
 
 
 def test():
@@ -201,14 +239,15 @@ def test():
         appid=LFASR_APP_ID,
         secret_key=LFASR_SECRETKEY,
         lyrics_dir=LYRICS_DIR,
-        lyrics_file_name='song_demo.txt',
-        upload_file_path=UPLOAD_FILE_DIR / 'song_demo.mp3',
+        lyrics_file_name="song_demo.txt",
+        upload_file_path=UPLOAD_FILE_DIR / "song_demo.mp3",
         download_dir=DOWNLOAD_DIR,
-        output_file_name=OUTPUT_JSON_NAME)
+        output_file_name=OUTPUT_JSON_NAME,
+    )
     print(getPerWordTime(result_json))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # loopExtractLyricsPartV1('fenHongSeDeHuiYi')
     # print(threadExtractLyricsPartV3('fenHongSeDeHuiYi'))
     # test_loop_vs_multithread()
