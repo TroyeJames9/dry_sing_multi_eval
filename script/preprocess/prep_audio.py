6ffe09da59fbd0b4e7eb585c32de9339884989a8
TroyeJames9 <130838331+TroyeJames9@users.noreply.github.com>
2024-01-26 10:48:35 +0800

    1.26_1

--- script/preprocess/prep_audio.py
+++ script/preprocess/prep_audio.py
@@ -21,17 +21,31 @@ from pathlib import Path
 
 # 音频数据由于设备不同，采样率也会不同，为方便研究，需统一采样率；
 # 单声道音频比处理立体声音频更简单，兼容性更好，占用更多的存储空间和计算资源更低
-def xfrVocalTract(audio_dir: Path = UPLOAD_FILE_DIR, dataset_name: str = None, audio_name: str = None,
-                  target_sr: Number = 16000,
-                  vt_num: Number = 1) -> int:
+def xfrVocalTract(
+    audio_dir: Path = UPLOAD_FILE_DIR,
+    dataset_name: str = None,
+    audio_name: str = None,
+    target_sr: int = 16000,
+    vt_num: int = 1,
+) -> np.ndarray:
     """按照target_sr进行重采样后，将声道数转化为指定数（一般项目需求为单声道）
-    pass
 
+    假设某一首歌的路径为 /Users/lijianxin/speech_recognition/audio/mp3/xxx.mp3
 
+    参数：
+        audio_dir：
+            默认根目录为setting文件中的UPLOAD_FILE_DIR，eg：audio文件的路径。
+        dataset_name：
+            音频文件除去根目录和音频名之间的部分，eg：mp3。
+        audio_name:
+            音频名,eg:xxx.mp3。
+        target_sr：
+            目标的采样率，16000为常用的采用率。
+        vt_num:
+            需要设置的声道数。
 
-
-
-
+    返回：
+        单声道音频文件的时域信号（一维的NumPy数组）y_resampled 和目标采样率target_sr。
     """
 
     if dataset_name is None:
@@ -55,24 +69,39 @@ def xfrVocalTract(audio_dir: Path = UPLOAD_FILE_DIR, dataset_name: str = None, a
     return y_resampled, target_sr
 
 
-def noiseReduce(y=None, sr=None, thresh_n_mult_nonstationary=2, y_noise=None, stationary=None):
-    reduced_noise = nr.reduce_noise(y=y,
-                                    sr=sr,
-                                    thresh_n_mult_nonstationary=thresh_n_mult_nonstationary,
-                                    y_noise=y_noise,
-                                    stationary=stationary)
+def noiseReduce(
+    y: np.ndarray = None,
+    sr: int = None,
+    thresh_n_mult_nonstationary: int = 2,
+    prop_decrease: float = 0.8,
+    stationary: bool = False,
+) -> np.ndarray:
+    """将重采样后的数据y_resampled进行降噪，消除非人声噪音.
+
+     noiseReduce库参数：https://github.com/timsainb/noisereduce
+
+     参数 ：
+         y:
+             加载的音频文件的时域信号，是一个一维的NumPy数组。
+         sr:
+             音频的采样率。
+         thresh_n_mult_nonstationary:
+             降噪的幅度，只能用于动态降噪中，数值越大，降噪的幅度越大。
+         prop_decrease:
+            降噪的比例，数字1表示降噪百分百。
+         stationary:
+             布尔值，True表示平稳降噪 ，False表示非平稳降噪。
+
+    返回：
+        降噪后的一维的NumPy数组的音频文件的时域信号。
+    """
+    reduced_noise = nr.reduce_noise(
+        y=y,
+        sr=sr,
+        n_std_thresh_stationary=1.5,
+        thresh_n_mult_nonstationary=thresh_n_mult_nonstationary,
+        prop_decrease=prop_decrease,
+        stationary=stationary,
+    )
 
     return reduced_noise
-
-
-y_resampled, target_sr = xfrVocalTract(dataset_name="mp3", audio_name="广东番禺中学附属学校_万可之.mp3")
-# audio_file = "/Users/lijianxin/speech_recognition/audio/mp3/广东番禺中学附属学校_万可之.mp3"
-# y, sr = librosa.load(audio_file, sr=None)
-
-a = noiseReduce(y=y_resampled, sr=target_sr, thresh_n_mult_nonstationary=3)
-print(a)
-
-# fig, ax = plt.subplots(figsize=(20, 3))
-# ax.plot(y_resampled)
-# ax.plot(y, alpha=1)
-# plt.show()
