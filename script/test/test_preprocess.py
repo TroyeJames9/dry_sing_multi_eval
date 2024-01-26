9aba4d09bdb36005bd7e3ec785ce75790bb1f448
TroyeJames9 <130838331+TroyeJames9@users.noreply.github.com>
2024-01-26 16:29:59 +0800

    添加_test_TestprepNotation

--- script/test/test_preprocess.py
+++ script/test/test_preprocess.py
@@ -4,11 +4,54 @@ import numpy as np
 import librosa
 from setting import *
 from preprocess.prep_audio import *
+from preprocess.prep_notation import *
+
+
+class TestprepNotation(unittest.TestCase):
+    def setUp(self) -> None:
+        # extractJson
+        self.json_dir = TEST_EIGEN_DIR
+        self.raw_data_dir = TEST_RAWDATA_DIR
+        self.json_name = "中华人民共和国国歌测试"
+        self.json_time_name = "中华人民共和国国歌_时间结果"
+        self.json_freq_name = "中华人民共和国国歌_最终结果"
+        self.freq_csv = FREQ_CSV
+
+        self.notation_dict = extractJson(
+            json_dir=self.json_dir, json_name=self.json_name
+        )
+        self.notation_dict_time = extractJson(
+            json_dir=json_dir, json_name=self.json_time_name
+        )
+        self.notation_dict_freq = extractJson(
+            json_dir=json_dir, json_name=self.json_freq_name
+        )
+
+        self.note_sig = "G"
+
+    def test_extractJson(self):
+        n_dict = extractJson(json_dir=self.json_dir, json_name=self.json_name)
+        assertIsNotNone(n_dict)
+
+    def test_calNoteTime(self):
+        t_dict = calNoteTime(eigen_dict=self.notation_dict)
+        # 节拍时间转化结果是否符合预期
+        assertDictEqual(t_dict, self.notation_dict_time)
+
+    def test_calNoteFreq(self):
+        f_dict = calNoteFreq(
+            eigen_dict_t=self.notation_dict_time,
+            data_dir=self.raw_data_dir,
+            data_name=FREQ_CSV,
+            note_sig=self.note_sig,
+        )
+        # 音符频率转化结果是否符合预期
+        assertDictEqual(f_dict, self.notation_dict_freq)
 
 
 class TestPrepAudio(unittest.TestCase):
     def setUp(self) -> None:
-        self.audio_dir = UPLOAD_FILE_DIR
+        self.audio_dir = TEST_AUDIO_DIR
         self.dataset_name = "qilai"
         self.audio_name = "qilai_1.mp3"
         self.target_sr = 16000
