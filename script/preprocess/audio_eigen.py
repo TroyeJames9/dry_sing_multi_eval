1dc916cbdded0b999642e9251d2d06a85b207409
TroyeJames9 <130838331+TroyeJames9@users.noreply.github.com>
2024-02-21 11:17:08 +0800

    jx2024.2.21_2

--- script/preprocess/audio_eigen.py
+++ script/preprocess/audio_eigen.py
@@ -10,6 +10,7 @@ from setting import *
 import json
 import pandas as pd
 import numpy as np
+import librosa
 
 
 def audioWordSeg(
@@ -69,3 +70,31 @@ def audioWordSeg(
     eigen_list = {"eigen_list": eigen_segments}
 
     return eigen_list
+
+
+def calAudioFreq(reduced_noise: np.ndarray, sr: int, fmax: float, fmin: float) -> list:
+    """使用Pyin算法来估计各时刻的基音频率，生成一个元组，包括基频列表和基频对应的times列表.
+
+    reduced_noise:
+        noiseReduce的返回结果。
+    sr：
+        noiseReduce的返回的采样率。
+    fmax：
+        估计的最大频率，默认为2093.0。
+    fmin:
+        估计的最小频率，默认为65.0。
+
+    返回：
+        Freq_list：
+            基频列表。
+        times_list：
+            各基频对应的times的列表。
+    """
+    # Freq_list 以赫兹为单位的基频时间序列。voiced_flag 包含指示帧是否有声的布尔标志的时间序列；voiced_probs 包含帧有声概率的时间序列。
+    Freq_list, voiced_flag, voiced_probs = librosa.pyin(
+        y=reduced_noise, sr=sr, fmin=fmin, fmax=fmax
+    )
+
+    times_list = librosa.times_like(Freq_list)
+
+    return Freq_list, times_list
