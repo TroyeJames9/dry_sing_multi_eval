1c462ed4d58cb2288db31925f0bf30650054b7c0
TroyeJames9 <130838331+TroyeJames9@users.noreply.github.com>
2024-02-22 10:28:42 +0800

    qy_添加_batchCsv2Pd

--- script/result/dataframe_op.py
+++ script/result/dataframe_op.py
@@ -3,10 +3,21 @@
 """本模块根据整体框架设计而对data文件夹的数据进行合并、计算，最终得到指标结果表
 
 """
+import pandas as pd
+from pathlib import Path
 
 
-FILE = Path(__file__).resolve()
-ROOT = FILE.parents[1]  # program root directory
-if str(ROOT) not in sys.path:
-    sys.path.append(str(ROOT))  # add ROOT to PATH
-ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+def batchCsv2Pd(csv_dir, encoding='ANSI'):
+    """
+    批量读取指定路径下的CSV文件，并返回一个字典。
+    参数：
+    param csv_dir: 文件的绝对路径，Path对象
+    返回
+    csv_d:键名为文件名（不包括扩展名），值为对应的pd.DataFrame
+    """
+    csv_d = {}  # 初始化结果字典
+    for csv_file in csv_dir.glob('*.csv'):  # 遍历目录下的所有CSV文件
+        df = pd.read_csv(csv_file, encoding=encoding)  # 读取CSV文件为DataFrame
+        file_name = csv_file.stem  # 获取文件名（不包括扩展名）
+        csv_d[file_name] = df  # 将DataFrame存储在字典中，键为文件名
+    return csv_d
