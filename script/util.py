017eabcfb0686769c83cd5cc935ef16045c15b42
TroyeJames9 <130838331+TroyeJames9@users.noreply.github.com>
2024-03-06 16:39:26 +0800

    创建_util.py
    Signed-off-by: TroyeJames9 <130838331+TroyeJames9@users.noreply.github.com>

--- script/util.py
+++ script/util.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+import concurrent.futures
+import multiprocessing
+import psutil
 from setting import *
 
 
+def multipuleProcess(
+    func_name=None,
+    arg_list: list = None,
+    max_workers: int = psutil.cpu_count(logical=False) // 2,
+    chunksize: int = None
+):
+    with concurrent.futures.ProcessPoolExecutor(max_workers) as executor:
+        if chunksize is None:
+            chunksize = len(arg_list) // max_workers // 20
+            if chunksize <= 1:
+                chunksize = 1
+        result_list = list(executor.map(func_name, arg_list, chunksize=chunksize))
+
+    return result_list
